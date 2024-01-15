38e76742032e445c018e7de2f2a438fb8147861f
Shantanu Patnaik <patnaikshantanu@gmail.com>
2024-01-15 22:33:22 +0530

    added logic to take care of white space in admin emails

--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -12,7 +12,7 @@ const authenticateAdmin = async (req, res, next) => {
     }
 
     const admin_emails_str = process.env.ADMINS;
-    const admin_emails = admin_emails_str.split(',');
+    const admin_emails = admin_emails_str.split(',').trim(e => e.trim());;
     if(!admin_emails.includes(user.email)){
       throw new Error('Unauthorized!')
     }
